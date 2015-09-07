b98218ffe9a351563cc515fa83c2157205bf747d
Samarth Kapoor <samarth003@e.ntu.edu.sg>
2015-09-07 13:04:48 +0800

    integrating solar radiation with wind sensor

--- ida_demo/ida_demo.ino
+++ ida_demo/ida_demo.ino
@@ -22,6 +22,12 @@ void loop() {
   float wData;
   float wAngleData;
   float wTempData;
+
+  //variables for SP 2015
+  int solar_sens_Value;
+  float solar_val;
+  //char charVal_solar[6];            
+  //String stringVal_solar = "";
   
   while(!mySerial.available()){
     //loop until mySerial is available
@@ -77,9 +83,23 @@ void loop() {
   Serial.println(wData);
   Serial.println(wAngleData);
   Serial.println(wTempData);
+  memset(inputString,0,sizeof(inputString)); //clear the character array
 
-  
-  memset(inputString,0,sizeof(inputString)); //clear the character array      
+  //Solar radiation SP 2015 data extraction     
+  solar_sens_Value = analogRead(A1);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 3.3V), multiplication factor of 1.52 to convert to a voltage (0 - 5V)
+  //float solar_voltage = solar_sens_Value * (3.3 / 1023.0)*1.52;
+  solar_val = solar_sens_Value * 0.25;
+/*  dtostrf(solar_val, 2, 2, charVal_solar);  //4 is mininum width, 4 is precision; float value is copied onto buff
+  for(int i=0;i<sizeof(charVal_solar);i++){
+    //Serial.print(charVal_temp[i]);
+  }
+  //convert chararray to string
+  for(int i=0;i<sizeof(charVal_solar);i++){
+    stringVal_solar+=charVal_solar[i];
+  }
+*/
+Serial.println(solar_val);        
 }
 
 
